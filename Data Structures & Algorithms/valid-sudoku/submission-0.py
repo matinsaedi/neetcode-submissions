66cf14c3bb97aca5a5792a6fe0af8c25f6f6dbad
Matin Saedi <matinsaedi23@gmail.com>
2026-06-23 14:23:25 -0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,49 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        # Row check
+        
+        for i in range(9):
+            visited = set()
+            row = board[i]
+            for val in row:
+                if val != ".":
+                    if val in visited:
+                        return False
+                    
+                    else:
+                        visited.add(val)
+
+        # Column check
+        for j in range(9):
+            visited = set()
+            for i in range(9):
+                val = board[i][j]
+                if val != ".":
+                    if val in visited:
+                        return False
+                    
+                    else:
+                        visited.add(val)
+
+        # 3*3 Box check
+        for start_row in range(0, 9, 3):
+            for start_column in range(0, 9, 3):
+                visited = set()
+                for i in range(start_row, start_row+3):
+                    for j in range(start_column, start_column+3):
+                        val = board[i][j]
+                        if val != ".":
+                            if val in visited:
+                                return False
+                            
+                            else:
+                                visited.add(val)
+
+        return True
+
+
+        
+
+        
+        
