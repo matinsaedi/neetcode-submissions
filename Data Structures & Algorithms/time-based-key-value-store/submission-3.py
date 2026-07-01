2008fb143d7fb011bf244407c223740dbdbcf220
Matin Saedi <matinsaedi23@gmail.com>
2026-07-01 19:28:52 -0400

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,39 @@
+class TimeMap:
+
+    def __init__(self):
+        self.map = dict()
+        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.map:
+            entry = (timestamp, value)
+            self.map[key].append(entry)
+
+        else:
+            self.map[key] = [(timestamp, value)]
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+
+        if key not in self.map:
+            return ""
+
+        data = self.map[key]
+
+        l, r = 0, len(data) - 1
+
+        result = ""
+        while l <= r:
+            m = (l + r) // 2
+
+            if data[m][0] == timestamp:
+                return data[m][1]
+
+            elif data[m][0] < timestamp:
+                result = data[m][1]
+                l = m + 1
+
+            else:
+                r = m - 1
+
+        
+        return result
