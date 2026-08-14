3a480dc4e246e8542c36fe153ba211be584cbdf7
Matin Saedi <matinsaedi23@gmail.com>
2026-08-14 14:03:28 -0400

    Add: count-squares - submission-0

---
+++ Data Structures & Algorithms/count-squares/submission-0.py
@@ -0,0 +1,31 @@
+class CountSquares:
+
+    def __init__(self):
+        self.points = {}
+
+    def add(self, point: List[int]) -> None:
+        point = tuple(point)
+        if point in self.points:
+            self.points[point] += 1
+
+        else:
+            self.points[point] = 1
+        
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+        px, py = point
+
+        for p in self.points:
+            x, y = p
+            if px == x or py == y:
+                continue
+            
+            if (px, y) in self.points:
+                if (x, py) in self.points:
+                    res += (self.points[(x, y)] * 
+                            self.points[(px, y)] *
+                            self.points[(x, py)]
+                    )
+        
+        return res
